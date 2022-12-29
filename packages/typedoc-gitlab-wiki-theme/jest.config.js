ec125fa18868ebec3938fcfd47b07b59608436be
Michael Aldape <michael.aldape@flosports.tv>
2022-12-29 10:35:14 -0600

    remove unnecessary examples and packages

--- packages/typedoc-gitlab-wiki-theme/jest.config.js
+++
@@ -1,7 +0,0 @@
-const base = require('../../jest.config.base.js');
-
-module.exports = {
-  ...base,
-  name: 'typedoc-gitlab-wiki-theme',
-  displayName: 'typedoc-gitlab-wiki-theme',
-};
--- packages/typedoc-gitlab-wiki-theme/src/index.ts
+++
@@ -1,7 +0,0 @@
-import { Application } from 'typedoc';
-
-import { GitlabWikiTheme } from './theme';
-
-export function load(app: Application) {
-  app.renderer.defineTheme('gitlab-wiki', GitlabWikiTheme);
-}
